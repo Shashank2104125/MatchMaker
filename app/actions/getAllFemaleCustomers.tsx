105b390c16ca783e4436ce59f72a292023824541
Shashank Yadav <97789487+Shashank2104125@users.noreply.github.com>
2025-08-10 00:48:15 +0530

    handle static data issue

--- app/actions/getAllFemaleCustomers.tsx
+++ app/actions/getAllFemaleCustomers.tsx
@@ -1,10 +1,7 @@
 'use server'
 
-import { promises as fs } from 'fs';
-import path from 'path';
+import femaleCustomers from "@/data/femaleCustomers.json"
 
 export async function getFemaleCustomers() {
-  const filePath = path.join(process.cwd(), 'data', 'femaleCustomers.json');
-  const data = await fs.readFile(filePath, 'utf-8');
-  return JSON.parse(data);
+  return femaleCustomers
 }
--- app/actions/getAllMaleCustomers.tsx
+++ app/actions/getAllMaleCustomers.tsx
@@ -1,10 +1,7 @@
 'use server'
 
-import { promises as fs } from 'fs';
-import path from 'path';
+import maleCustomers from '@/data/maleCustomers.json';
 
 export async function getMaleCustomers() {
-  const filePath = path.join(process.cwd(), 'data', 'maleCustomers.json');
-  const data = await fs.readFile(filePath, 'utf-8');
-  return JSON.parse(data);
+  return maleCustomers;
 }
